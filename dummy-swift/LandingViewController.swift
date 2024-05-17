5d34ed1db1c8adf286c001c6e827e96feef12f44
Robert Wan <wantunglok@gmail.com>
2024-05-17 22:05:11 +1000

    add confirmation dialog

--- dummy-swift/LandingViewController.swift
+++ dummy-swift/LandingViewController.swift
@@ -10,16 +10,23 @@ import UIKit
 
 class LandingViewController: ViewController {
 
+    var confirmationDialog: ConfirmationDialog? = nil
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Landing"
         
-        let overlayView = addOverlayView(self)
-        overlayView.dummyLabel.text = "Good"
-//        overlayView.removeFromSuperview()
+        confirmationDialog = getConfirmationDialog(self)
+        confirmationDialog?.label.text = "Error"
+        confirmationDialog?.button.setTitle("OK", for: .normal)
+        confirmationDialog?.button.addTarget(self, action: #selector(self.buttonClicked), for: .touchUpInside)
     }
     
     @IBAction func onWeatherButtonClicked(_ sender: Any) {
         navigationController?.pushViewController(WeatherViewController(), animated: true)
+    }
+    
+    @objc func buttonClicked() {
+        confirmationDialog?.removeFromSuperview()
     }
 }
